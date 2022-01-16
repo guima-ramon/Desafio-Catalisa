8b91c7b924f866f6832ff5ece2f6eff44f46547c
ramon guimaraes <pramonmg@gmail.com>
2022-01-16 20:14:36 -0300

    Adicionar html finalizado

--- script.js
+++ script.js
@@ -1,29 +1,93 @@
-const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
+const botao = document.querySelector('button')
 
-gerarValorAleatorio = () => {
-    return Math.floor(Math.random() * 671);
+const imagem1 = document.querySelector('img#personagem1')
+const nomeDoPersonagem1 = document.querySelector('#nome1')
+const especie1 = document.querySelector('#especie1')
+const condicao1 = document.querySelector('#status1')
+
+const imagem2 = document.querySelector('img#personagem2')
+const nomeDoPersonagem2 = document.querySelector('#nome2')
+const especie2 = document.querySelector('#especie2')
+const condicao2 = document.querySelector('#status2')
+
+const imagem3 = document.querySelector('img#personagem3')
+const nomeDoPersonagem3 = document.querySelector('#nome3')
+const especie3 = document.querySelector('#especie3')
+const condicao3 = document.querySelector('#status3')
+
+traduzirCondicao = data => { 
+  if (data.status == 'unknown') {
+    return 'Não sabemos'
+  } else if (data.status == 'Alive') {
+    return 'Sim'
+  } else {
+    return 'Não. Está morto'
+  }
 }
 
-pegarPersonagem = () => {
-    let numerAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numerAleatorio}`, {
-        method:'GET',
-        headers: {
-            Accept: 'application/json',
-            "Content-type": 'application/json'
-        }
-    }).then((response) => response.json()).then((data) => {
-        imagem.src= data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        condicao.innerHTML = data.status;
-
-    });
+gerarValorAletorio = () => {
+  return Math.floor(Math.random() * 671)
 }
 
-botao.onclick = pegarPersonagem;
+pegarPersonagem1 = () => {
+  let numeroAleatorio = gerarValorAletorio()
+  return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      imagem1.src = data.image
+      imagem1.alt = data.name
+      nomeDoPersonagem1.innerHTML = data.name
+      especie1.innerHTML = data.species
+      condicao1.innerHTML = traduzirCondicao(data)
+    })
+}
+
+pegarPersonagem2 = () => {
+  let numeroAleatorio = gerarValorAletorio()
+  return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      imagem2.src = data.image
+      imagem2.alt = data.name
+      nomeDoPersonagem2.innerHTML = data.name
+      especie2.innerHTML = data.species
+      condicao2.innerHTML = traduzirCondicao(data)
+    })
+}
+
+pegarPersonagem3 = () => {
+  let numeroAleatorio = gerarValorAletorio()
+  return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      imagem3.src = data.image
+      imagem3.alt = data.name
+      nomeDoPersonagem3.innerHTML = data.name
+      especie3.innerHTML = data.species
+      condicao3.innerHTML = traduzirCondicao(data)
+    })
+}
+
+botao.onclick = () => {
+  pegarPersonagem1()
+  pegarPersonagem2()
+  pegarPersonagem3()
+}
